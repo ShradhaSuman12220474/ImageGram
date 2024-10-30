dedcccb3fffd3ac34a040f89f9c7982f8f4cc874
suman <isuman0007@gmail.com>
2024-10-30 17:06:55 +0530

    Added nodemon for auto restart

--- src/config/awsConfig.js
+++ src/config/awsConfig.js
@@ -2,10 +2,10 @@ import AWS from 'aws-sdk';
 import { AWS_ACCESS_KEY_ID, AWS_REGION, AWS_SECRET_ACCESS_KEY_ID } from './serverConfig.js';
 
 
-console.log("region: ",AWS_REGION);
-console.log("scret access key: ",AWS_SECRET_ACCESS_KEY_ID);
+// console.log("region: ",AWS_REGION);
+// console.log("scret access key: ",AWS_SECRET_ACCESS_KEY_ID);
 
-console.log("access key:",AWS_ACCESS_KEY_ID);
+// console.log("access key:",AWS_ACCESS_KEY_ID);
 export const s3 = new AWS.S3({
     region: AWS_REGION,
     accessKeyId: AWS_ACCESS_KEY_ID,
